ca4959a03003eae04659133008e7dc8135feea14
TerryMinal <tguan@stuy.edu>
2017-10-09 11:58:41 -0400

    finished app.py with redirect

--- app.py
+++ app.py
@@ -8,30 +8,33 @@ import os
 app = Flask(__name__)
 
 app.secret_key = os.urandom(32)
-usr = ""
-
+usr = "KenM"
+passw = "1234"
+method = ""
 #root route pulls up login template
 @app.route("/")
-def hi():
+def home():
     if "username" in session:
-        return redirect(url_for("process"))
-    return render_template('login.html', message = "")
+        return redirect(url_for("welcome"))
+    return render_template("login.html", message = "")
 
 @app.route("/welcome")
 def welcome():
-    
-
+    if "username" not in session:
+        return render_template("login.html", message = "")
+    else:
+        return render_template("welcome.html", name = usr, method= method)
 #auth checks for the correct username and password
 @app.route("/auth", methods = ["POST"])
-def process():
+def auth():
     person = request.form["User"] #gets the entered username
     pw = request.form["Pass"] #same for pw
-    reqmeth = request.method
-    if (person == "KenM"): #checks for correct info
-        global usr = person
-        if (pw == "1234"):  
+    global method
+    method = request.method
+    if (person == usr): #checks for correct info
+        if (pw == passw):
             session["username"] = person
-            return render_template('welcome.html', name = person, method = reqmeth) #if theinfo is good, renders the welcome
+            return redirect(url_for("welcome"))
         else: #if user was correct but pw incorrect, back to login with message
             return render_template('login.html', message = "Invalid Password")
     else: #if ser incorrect, back to login with message
@@ -40,8 +43,7 @@ def process():
 @app.route("/logout")
 def logout(): #ends session and brings back to login with message
     session.pop("username")
-    return render_template('login.html', message = "Successfully logged out!")
-
+    return redirect(url_for("welcome"))
 if __name__ == "__main__":
     app.debug = True
     app.run()
